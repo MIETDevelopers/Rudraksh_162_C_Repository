2b9c5830c5458721e4ed75923b59115daeb601b9
Rudraksh Vasudev <2020a1r162@mietjammu.in>
2021-03-17 15:40:37 +0530

    sum

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("\nEnter the value of a: ");
+	scanf("%d",&a);
+	printf("\nEnter the value of b: ");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("\nSum of %d + %d = %d",a,b,sum);
+}
