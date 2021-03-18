9d75fceaf422c5bb5ccfce895ed4a2f588dcd179
Rudraksh Vasudev <2020a1r162@mietjammu.in>
2021-03-18 15:16:30 +0530

    comment

--- sum.c
+++ sum.c
@@ -1,11 +1,15 @@
+//Author: Rudraksh VAsudev
+//Creation Date: 17/03/2021
+//Last Modification:18/03/2021
+//Purpose:To Add Two Numbers and Print Their Sum
 #include<stdio.h>
-int main()
+int main()	//Main Body Fuinction	
 {
-	int a,b,sum;
-	printf("\nEnter the value of a: ");
-	scanf("%d",&a);
-	printf("\nEnter the value of b: ");
-	scanf("%d",&b);
-	sum=a+b;
-	printf("\nSum of %d + %d = %d",a,b,sum);
+	int a,b,sum;	//Data type and Variable Declaration And Initialisation 
+	printf("\nEnter the value of a: "); 	//printf function
+	scanf("%d",&a); 	//output function
+	printf("\nEnter the value of b: "); 	//printf function call
+	scanf("%d",&b); 	//output function
+	sum=a+b; 	//variable declaration
+	printf("\nSum of %d + %d = %d",a,b,sum); 	//printf function
 }
--- test_hello.c
+++ test_hello.c
@@ -1,5 +1,9 @@
+//Author:Rudraksh Vasudev
+//Creation Date: 17/03/2021
+//Date Of Modification:18/03/2021
+//Objective:To Print "Hello World"
 #include<stdio.h>
-int main()
+int main()		//Main Function Body
 {
-	printf("Hello World");
+	printf("Hello World"); 		//Printf Function
 }
