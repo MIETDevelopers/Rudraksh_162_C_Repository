7088b2d4fca09611de4e593b8e5d0e7909d9d24c
rudrakshvasudev <76394801+rudrakshvasudev@users.noreply.github.com>
2021-04-04 23:04:54 +0530

    Update 27_StoreMarks.c

--- 27_StoreMarks.c
+++ 27_StoreMarks.c
@@ -1,3 +1,4 @@
+//Author: Rudraksh Vasudev
 //A C Program to store the information of student marks using structures and find total marks of individual student.
 #include <stdio.h>
 struct student {
